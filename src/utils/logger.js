5b4e7efbd3cddda802d0683f3e0ad5acc33f15b1
Jaikarans <jaikrns@gmail.com>
2025-02-02 16:08:13 +0530

    fix: Set logger timestamp to Indian Standard Time

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,9 +1,26 @@
-import {createLogger, format, transports } from 'winston';
+import {createLogger, format, transports} from 'winston';
 import { NODE_ENV } from '../config/config.js';
 
 export const logger = createLogger({
     level: 'info',
-    format: format.json(),
+    format: format.combine(
+        format.timestamp({
+            format: () => {
+                const date = new Date();
+                return new Intl.DateTimeFormat('en-IN', {
+                  timeZone: 'Asia/Kolkata',  // UTC+5:30 (India Standard Time)
+                  year: 'numeric',
+                  month: '2-digit',
+                  day: '2-digit',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  second: '2-digit',
+                }).format(date);
+              }
+        
+        }),
+        format.json(),
+    ),
     defaultMeta: {service: 'server', environment: `${NODE_ENV}`},
     transports: [
         new transports.File({ filename: './logs/error.log', level: 'error' }),

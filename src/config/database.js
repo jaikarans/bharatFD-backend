ee0a4094c09e24e4a2b96bcae8990d535606628e
Jaikarans <jaikrns@gmail.com>
2025-02-02 19:29:20 +0530

    fix: Retry to connect mongo if connection not establised

--- src/config/database.js
+++ src/config/database.js
@@ -4,11 +4,16 @@ import { logger } from '../utils/logger.js';
 
 const mongoUri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}`;
 
+const connectWithRetry = () => {
+    mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            logger.log('info', 'Connection established to MongoDB');
+        })
+        .catch((e) => {
+            logger.log('error', `MongoDB connection error: ${e.message}`);
+            // Retry connection after every 5 seconds
+            setTimeout(connectWithRetry, 5000);
+        });
+};
 
-mongoose.connect(mongoUri)
-    .then(() => {
-        logger.log('info', 'Connection establised to MongoDB');
-    })
-    .catch((e) => {
-        logger.log('error', `MongoDB connection error: ${e.message}`);
-    });
+connectWithRetry();
